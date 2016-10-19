7df0cbf812127a3d3b75c885bb511f82529824eb
Jose Berrocal <jlberrocal@users.noreply.github.com>
2016-10-19 04:45:02 -0600

    feat(file-upload): Add the possibility of set the token header (#213)
    It will be very useful set which is the Token that is going to be send to the server

--- components/file-upload/file-uploader.class.ts
+++ components/file-upload/file-uploader.class.ts
@@ -60,6 +60,7 @@ export class FileUploader {
     this.options = (Object as any).assign(this.options, options);
 
     this.authToken = options.authToken;
+    this.authTokenHeader = options.authTokenHeader || 'Authorization';
     this.autoUpload = options.autoUpload;
     this.options.filters.unshift({name: 'queueLimit', fn: this._queueLimitFilter});
 
@@ -353,7 +354,7 @@ export class FileUploader {
       }
     }
     if (this.authToken) {
-      xhr.setRequestHeader('Authorization', this.authToken);
+      xhr.setRequestHeader(this.authTokenHeader, this.authToken);
     }
     xhr.send(sendable);
     this._render();
